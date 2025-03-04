ac17c4b72054c707023af5e4de25215688a171d9
dinesh-saini814 <dm43102154@gmail.com>
2025-03-05 00:26:58 +0530

    final commit

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -29,11 +29,9 @@ export async function GET() {
     });
   }
 
-  // return NextResponse.redirect(
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://blogits.vercel.app/dashboard"
-  //     : "http://localhost:3000/dashboard"
-  // );
-
-  return NextResponse.redirect("https://blogits.vercel.app/dashboard");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "production"
+      ? "https://blogits.vercel.app/dashboard"
+      : "http://localhost:3000/dashboard"
+  );
 }
